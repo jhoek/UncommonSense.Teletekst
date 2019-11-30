97c55cd708e8f3f2c85aace29d6f2472519425d6
Jan Hoek <github@uncommonsense.nl>
2019-11-30 12:03:28 +0100

    More work on sanitizing index

--- Get-TeletekstDomesticNews.ps1
+++ Get-TeletekstDomesticNews.ps1
@@ -19,12 +19,21 @@ function Get-TeletekstDomesticNews
     param
     (
     )
+    #| Where-Object { $_ -match '<span class="yellow ">\s*(.*)<a class="yellow" href=".*">(\d+)</a></span>' } `
 
     Invoke-RestMethod -Uri 'http://teletekst-data.nos.nl/json/103' `
     | Select-Object -ExpandProperty content `
-    | ForEach-Object { $_ -split "`n" } `
-    | Where-Object { $_ -match '<span class="yellow ">\s*(.*)<a class="yellow" href=".*">(\d+)</a></span>' } `
-    | ForEach-Object { [PSCustomObject]@{ Title = NormalizeTitle($Matches[1]); Page = $Matches[2] } }    
+    | ForEach-Object { ($_ -split "`n").Trim() } `
+    | Where-Object { $_ } `
+    | ForEach-Object { 
+        $Title = [regex]::Match($_, '^<span class="yellow ">(.*?)<')
+        $PageNo = [regex]::Match($_, '(\d{3})</a></span>$')
+
+        if ($Title.Success -and $PageNo.Success)
+        {
+            [PSCustomObject]@{ Title = NormalizeTitle($Title.Groups[1].Value.Trim()); Page = $PageNo.Groups[1].Value.Trim() } 
+        }    
+    }
 }
 
 Get-TeletekstDomesticNews
