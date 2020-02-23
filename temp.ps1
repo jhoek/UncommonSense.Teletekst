039eecae0705d4fda1571357090fc95f27b0799b
Jan Hoek <github@uncommonsense.nl>
2020-02-23 18:31:32 +0100

    More work on news content

--- temp.ps1
+++ temp.ps1
@@ -1,8 +1,19 @@
-$Text = Invoke-WebRequest -Uri 'https://teletekst-data.nos.nl/webplus?p=121-1' `
-| Select-Object -ExpandProperty Content `
-| pup '.cyan text{}' --plain `
-| ForEach-Object { $_ -replace '^\s*', '' } `
-| Select-Object -SkipLast 1 `
-| Join-String -Separator ' '
+104..110 `
+| ForEach-Object {
+    $Content = Invoke-WebRequest -Uri "https://teletekst-data.nos.nl/webplus?p=$($_)-1" | Select-Object -ExpandProperty Content 
+    $Title = ($Content | pup '.doubleHeight text{}' --plain | Join-String -Separator ' ').Trim()
+    $Text = ($Content | pup '.cyan text{}' --plain `
+        | ForEach-Object { $_ -replace '^\s*', '' } `
+        | Select-Object -SkipLast 1 `
+        | Join-String -Separator ' ') -replace '\s{2,}', ' ' -replace '\.\s', '.' -replace '\.([A-Z])', '. $1' -replace ',(\S)', ', $1'
+    
+Write-Host $Title -ForegroundColor Cyan
+Write-Host $Text
+Write-Host
 
-$Text -replace '\s{2,}', ' ' -replace '\.\s', '.' -replace '\.([A-Z])', '. $1' -replace ',(\S)', ', $1'
+Send-PushoverNotification `
+    -ApplicationToken asxmmq8g95jt4ed1qcrucdvu2iuy67 `
+    -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+    -Title $Title `
+    -Message $Text
+}
