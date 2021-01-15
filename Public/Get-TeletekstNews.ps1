75ecf36b64ced4348360e494d867763fdb431280
Jan Hoek <github@uncommonsense.nl>
2021-01-15 14:11:29 +0100

    Remove trailing whitespace from title

--- Public/Get-TeletekstNews.ps1
+++ Public/Get-TeletekstNews.ps1
@@ -1,6 +1,12 @@
 function NormalizeTitle([string]$Text)
 {
+    # Remove leading whitespace
     $Text = $Text -replace '^\s*', ''
+
+    # Remove trailing whitespace
+    $Text = $Text -replace '\s*$',''
+
+    # Normalize remaining text
     $Text = NormalizeText($Text)
 
     $Text
