441bc80f00bad77daea0fb849778a10b45b928f1
Jan Hoek <jhoek@uncommonsense.nl>
2024-03-30 10:34:14 +0100

    Added tests and task

---
+++ UncommonSense.Teletekst.Tests.ps1
@@ -0,0 +1,17 @@
+Describe 'UncommonSense.Teletekst' {
+    It 'Returns <Type> news' {
+        $Result = Get-TeletekstNews -Type $Type
+        $Result.Length | Should -BeGreaterThan 3 -Because 'Normally at least 3 stories'
+
+        $Result.Foreach{
+            $_.Type | Should -Be $Type
+            $_.Page | Should -BeIn $Pages
+            $_.Title | Should -Not -BeNullOrEmpty
+            $_.Link | Should -Not -BeNullOrEmpty
+            $_.Content.Length | Should -BeGreaterThan 100
+        }
+    } -TestCases @(
+        @{Type = 'domestic'; Pages = 104..124 }
+        @{Type = 'foreign'; Pages = 125..137 }
+    )
+}
