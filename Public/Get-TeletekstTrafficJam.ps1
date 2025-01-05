079fc7438144354fb9f78016a5111b9956f6663f
Jan Hoek <github@uncommonsense.nl>
2025-01-05 19:48:38 +0100

    Get-TeletekstTrafficJam

---
+++ Public/Get-TeletekstTrafficJam.ps1
@@ -0,0 +1,25 @@
+function Get-TeletekstTrafficJam
+{
+    param
+    (
+    )
+
+    $DutchCulture = Get-Culture 'nl-NL'
+    $SubPage = 1
+    $PageData = Invoke-RestMethod -Uri "https://teletekst-data.nos.nl/json/730/$SubPage" -SkipHttpErrorCheck -StatusCodeVariable StatusCode
+
+    while($StatusCode -eq '200')
+    {
+        $Content = $PageData | Select-Object -ExpandProperty Content
+        $Document = ConvertTo-HtmlDocument -Text $Content
+
+        $DateTimeText = $Document | Select-HtmlNode -CssSelector '.cyan.bg-blue' -All | Select-Object -Skip 1 -First 1 | Get-HtmlNodeText
+        $DateTimeText = $DateTimeText -replace '\s', '' -replace '^actueel', '' -replace 'uur$', ''
+        $DateTime = [DateTime]::ParseExact($DateTimeText, 'ddMMM\.HH\:mm', $DutchCulture)
+        "**$($DateTime)**"
+        # $Document
+
+        $SubPage++
+        $PageData = Invoke-RestMethod -Uri "https://teletekst-data.nos.nl/json/730/$SubPage" -SkipHttpErrorCheck -StatusCodeVariable StatusCode
+    }
+}
--- UncommonSense.Teletekst.psd1
+++ UncommonSense.Teletekst.psd1
@@ -69,7 +69,7 @@
     # NestedModules = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = 'Get-TeletekstNews', 'Get-TeletekstWeather'
+    FunctionsToExport = 'Get-TeletekstNews', 'Get-TeletekstWeather', 'Get-TeletekstTrafficJam'
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport   = '*'
