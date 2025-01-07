169a75264829da8c9e1ec7de890190c917de939e
Jan Hoek <jhoek@uncommonsense.nl>
2025-01-07 20:36:35 +0100

    Preparations

--- Public/Get-TeletekstTrafficJam.ps1
+++ Public/Get-TeletekstTrafficJam.ps1
@@ -8,7 +8,9 @@ function Get-TeletekstTrafficJam
     $SubPage = 1
     $PageData = Get-TeletekstPage -Uri "https://teletekst-data.nos.nl/json/730-$SubPage"
 
-    while($PageData)
+    $PageData
+
+    while ($PageData)
     {
         $pageData[4]
         $DateTimeText = $PageData[4] -replace '\s', '' -replace '^actueel', '' -replace 'uur$', ''
