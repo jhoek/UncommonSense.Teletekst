00621374cbc9909a7cabf57758cc579b05d4199a
Jan Hoek <github@uncommonsense.nl>
2025-01-08 14:38:03 +0100

    More preparations

--- Private/Get-TeletekstPage.ps1
+++ Private/Get-TeletekstPage.ps1
@@ -12,26 +12,28 @@ function Get-TeletekstPage
         [int]$FooterLine = 0
     )
 
-    $Response = Invoke-RestMethod -Uri $Uri -SkipHttpErrorCheck
+    $Response = Invoke-RestMethod -Uri $Uri -SkipHttpErrorCheck -StatusCodeVariable StatusCode
 
-    $Content = $Response
-    | Select-Object -ExpandProperty Content
-    | ForEach-Object { $_ -replace '&#xF020;', '' }
-    | ForEach-Object { $_ -replace '&#xF02c;', '' }
-    | ForEach-Object { $_ -replace '&#xF02f;', '' }
-    | ForEach-Object { [System.Web.HttpUtility]::HtmlDecode($_) }
-    | ForEach-Object { $_ -replace '<span.*?>', '' }
-    | ForEach-Object { $_ -replace '</span>', '' }
-    | ForEach-Object { $_ -replace '<a.*?>', '' }
-    | ForEach-Object { $_ -replace '</a>', '' }
-    | ForEach-Object { $_ -split "`n"}
+    if ($StatusCode -eq 200){
+        $Content = $Response
+        | Select-Object -ExpandProperty Content
+        | ForEach-Object { $_ -replace '&#xF020;', '' }
+        | ForEach-Object { $_ -replace '&#xF02c;', '' }
+        | ForEach-Object { $_ -replace '&#xF02f;', '' }
+        | ForEach-Object { [System.Web.HttpUtility]::HtmlDecode($_) }
+        | ForEach-Object { $_ -replace '<span.*?>', '' }
+        | ForEach-Object { $_ -replace '</span>', '' }
+        | ForEach-Object { $_ -replace '<a.*?>', '' }
+        | ForEach-Object { $_ -replace '</a>', '' }
+        | ForEach-Object { $_ -split "`n"}
 
-    $Payload = $Content | Select-Object -Skip $HeaderLine -SkipLast $FooterLine
+        $Payload = $Content | Select-Object -Skip $HeaderLine -SkipLast $FooterLine
 
-    [PSCustomObject]@{
-        Content = $Content
-        Payload = $Payload
-        PrevPage = $Response.PrevPage
-        NextPage = $Response.NextPage
+        [PSCustomObject]@{
+            Content = $Content
+            Payload = $Payload
+            PrevPage = $Response.PrevPage
+            NextPage = $Response.NextPage
+        }
     }
 }
--- Public/Get-TeletekstTrafficJam.ps1
+++ Public/Get-TeletekstTrafficJam.ps1
@@ -6,15 +6,17 @@ function Get-TeletekstTrafficJam
 
     $DutchCulture = Get-Culture 'nl-NL'
     $SubPage = 1
-    $PageData = Get-TeletekstPage -Uri "https://teletekst-data.nos.nl/json/730-$SubPage"
+    $PageData = Get-TeletekstPage -Uri "https://teletekst-data.nos.nl/json/730-$SubPage" -HeaderLine 4 -FooterLine 3
 
     while ($PageData)
     {
-        $DateTimeText = $PageData.Content[4] -replace '\s', '' -replace '^actueel', '' -replace 'uur$', ''
+        $DateTimeText = $PageData.Payload[4] -replace '\s', '' -replace '^actueel', '' -replace 'uur$', ''
         $DateTime = [DateTime]::ParseExact($DateTimeText, 'ddMMM\.HH\:mm', $DutchCulture)
         "**$($DateTime)**"
 
+        $PageData.Payload
+
         $SubPage++
-        $PageData = Get-TeletekstPage -Uri "https://teletekst-data.nos.nl/json/730-$SubPage"
+        $PageData = Get-TeletekstPage -Uri "https://teletekst-data.nos.nl/json/730-$SubPage" -HeaderLine 4 -FooterLine 3
     }
 }
