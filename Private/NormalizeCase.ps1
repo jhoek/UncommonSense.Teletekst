98dcdcc00de722946284de3626bb6559517f1378
Jan Hoek <github@uncommonsense.nl>
2025-01-08 15:30:31 +0100

    More preparations

--- Private/NormalizeCase.ps1
+++ Private/NormalizeCase.ps1
@@ -1,4 +1,9 @@
 function NormalizeCase([string]$Text)
 {
-    [regex]::Replace($Text.ToLowerInvariant(), '^(\w)', { param($Match) $Match.Groups[1].Value.ToUpperInvariant() })
+    $Text = [regex]::Replace($Text.ToLowerInvariant(), '^(\w)', { param($Match) $Match.Groups[1].Value.ToUpperInvariant() })
+
+    $Text = $Text -replace 'limburg', 'Limburg'
+
+    $Text
+    # FIXME: rest
 }
---
+++ test.ps1
@@ -0,0 +1,5 @@
+Get-TeletekstNews
+
+Get-TeletekstWeather
+
+Get-TeletekstTrafficJam
