11c20fe752c3dd5610c2025da044beb36d3d28f2
Jan Hoek <jhoek@uncommonsense.nl>
2025-01-18 22:13:06 +0100

    Combine lines for traffic info

--- Public/Get-TeletekstTrafficJam.ps1
+++ Public/Get-TeletekstTrafficJam.ps1
@@ -19,12 +19,53 @@ function Get-TeletekstTrafficJam
         $PageData = Get-TeletekstPage -Uri "https://teletekst-data.nos.nl/json/730-$SubPage" -HeaderLine 6 -FooterLine 3 -SkipBlank
     }
 
+    $Content = $Content
+    | ForEach-Object `
+        -Begin {
+            $FirstSection = $true
+        } `
+        -Process {
+            $CurrentItem = $_
+
+            switch($true)
+            {
+                ($CurrentItem -like '*:' -or $CurrentItem -like '- *')
+                {
+                    # Flush buffer
+                    if ($Buffer) { $Buffer }
+                    $Buffer = ''
+                }
+
+                ($CurrentItem -like '*:')
+                {
+                    if (-not $FirstSection) { '' }
+                    $CurrentItem
+                    $FirstSection = $false
+                    break
+                }
+
+                ($CurrentItem -like '- *')
+                {
+                    $Buffer = $CurrentItem
+                    break
+                }
+
+                default
+                {
+                    $Buffer = $Buffer + ' ' + $CurrentItem
+                }
+            }
+        } `
+        -End {
+            if ($Buffer) { $Buffer }
+        }
+
     [PSCustomObject]@{
         Page       = 730
         DateTime   = $DateTime
         Link       = 'https://nos.nl/teletekst#730'
         Title      = 'Verkeersinformatie'
-        Content    = NormalizeText($Content -join ' ')
+        Content    = NormalizeText($Content -join "`n")
         PSTypeName = 'UncommonSense.Teletekst.Traffic'
     }
 
